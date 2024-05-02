ec6e182a9e2227cb0b7958da9963857b2ae060d8
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-02 15:59:21 +0200

    finally imported three and other stuff

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from './node_modules/three/build/three.module.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
+
 
 let col = document.getElementsByClassName('second-col')[0]
 
 let scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
 renderer.setSize( window.innerWidth, window.innerHeight );
 col.appendChild( renderer.domElement );
 
