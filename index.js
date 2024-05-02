d087c91d98a58e321b0d9ab396b3d81aa89a234b
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-02 15:33:00 +0200

    npm installed three

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import * as THREE from 'three';
+import * as THREE from './three.module.js';
 
 let col = document.getElementsByClassName('second-col')[0]
+
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+renderer.setSize( window.innerWidth, window.innerHeight );
+col.appendChild( renderer.domElement );
+
+console.log(col)
