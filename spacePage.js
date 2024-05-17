7e9fb94758b8331b4cb31a8acb6e9e26890506f1
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-17 09:50:51 +0200

    start with steps

--- spacePage.js
+++ spacePage.js
@@ -10,7 +10,6 @@ const renderer = new THREE.WebGLRenderer({alpha:true});
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 renderer.setSize(window.innerWidth, window.innerHeight );
 canvas.appendChild( renderer.domElement );
-renderer.setClearColor(0x999999)
 let pointLights=[]
 
 
